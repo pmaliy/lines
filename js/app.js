c28ddc12de7f54252b36d66bd857a4b11ad7eef7
Pavel Maliy <pmaliy.ua@gmail.com>
2018-04-24 10:47:05 +0000

    LinesStorage with importJSON()

--- js/app.js
+++ js/app.js
@@ -25,47 +25,56 @@ function Rest(base) {
   };
 }
 
-const db = new Rest(DB_URL_BASE);
+function LinesStorage(db) {
+  // 2-way connections map
+  const keysMap = {
+    definitions: 'terms',
+    examples: 'subjects',
+    tags: 'links'
+  };
+  
+  const saveLine = data => {
+    const keys = Object.keys(data);
+    if (keys.length === 1) {
+//      console.log('POST');
+//      console.log(data);
+      return db.post('lines', data);
+    }
+    const chunkPromises = [];
+    for (let i = 0, key = keys[i]; i < keys.length; ++i, key = keys[i]) {
+      if (key === 'value') continue;
+      chunkPromises.push(saveLines(data[key]).then(ids => ({
+        [key]: ids.reduce((hash, { name: id }) => ({ ...hash, [id]: true }), {})
+      })));
+    }
+    return Promise.all(chunkPromises)
+      .then(chunks => db.post('lines', chunks.reduce((mix, chunk) => ({ ...mix, ...chunk }), data)))
+//      .then(id => );
+  };
 
+  const saveLines = data => Promise.all(data.map(chunk => saveLine(chunk)));
+  
+  return {
+    importJSON: json => Array.isArray(json) ? saveLines(json) : saveLine(json),
+    get: id => db.get('lines' + (id ? `/${id}` : ''))
+  }
+}
+
+const db = new Rest(DB_URL_BASE);
 //db.post('lines', {fizz: 'buzz'}).then(() => db.get('lines')).then(console.log);
 //db.get('lines').then(console.log);
 
-const extractLines = chunk => {
-  console.log(chunk);
-  
-  const res = [];
-  for (let key in chunk) {
-    if (key === 'value') continue;
-    res.push(chunk[key]);
-  }
-  return res;
-};
+const lines = new LinesStorage(db);
+//lines.get().then(console.log);
 
-const getLinesData = () => fetch('data/lines.json' + location.search).then(response => response.json());
+// lines db cleanup
+//db.put('lines', { foobar: true });
 
-const saveLine = data => {
-  const keys = Object.keys(data);
-  if (keys.length === 1) {
-    console.log('POST');
-    console.log(data);
-    return db.post('lines', data);
-  }
-  const chunkPromises = [];
-  for (let i = 0, key = keys[i]; i < keys.length; ++i, key = keys[i]) {
-    if (key === 'value') continue;
-    chunkPromises.push(saveLines(data[key]).then(ids => ({
-      [key]: ids.reduce((hash, id) => ({ ...hash, [id.name]: true }), {})
-    })));
-  }
-  return Promise.all(chunkPromises)
-    .then(chunks => db.post('lines', chunks.reduce((mix, chunk) => ({ ...mix, ...chunk }), data)));
-};
-
-const saveLines = data => Promise.all(data.map(chunk => saveLine(chunk)));
-
-//getLinesData()
-////  .then(data => data.slice(0, 2))
-//  .then(saveLines)
+// lines json import to db
+//const getLinesJSON = () => fetch('data/lines.json' + location.search).then(data => data.json());
+//getLinesJSON()
+//  .then(json => json.slice(0, 2))
+//  .then(lines.importJSON)
 //  .then(console.log);
 
 
